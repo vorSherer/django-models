0c3517115f17496533bf97dc95eee6fa8f0484d5
vorSherer <tcpma2011@gmail.com>
2020-08-26 21:17:52 -0700

    DRYed Django tests.

--- blog/tests.py
+++ blog/tests.py
@@ -4,24 +4,25 @@ from django.urls import reverse
 
 class BlogTests(SimpleTestCase):
 
-    def test_home_page_status(self):
-        url = reverse('home')
+    def helper_page_status_200(self, url_name):
+        url = reverse(url_name)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def helper_page_template_check(self, url_name):
+        url = reverse(url_name)
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, f'{url_name}.html')
+
+    def test_home_page_status(self):
+        self.helper_page_status_200('home')
 
     def test_home_page_template(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertTemplateUsed(response, 'base.html')
-        self.assertTemplateUsed(response, 'home.html')
+        self.helper_page_template_check('home')
 
     def test_check_page_status(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.helper_page_status_200('check')
 
     def test_check_page_template(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertTemplateUsed(response, 'base.html')
-        self.assertTemplateUsed(response, 'home.html')
+        self.helper_page_template_check('check')
